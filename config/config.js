8ccb985a5e01fdfcafb32a5318a5fcc7bfa6a1df
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2014-02-18 18:52:26 -0600

    a little cleaning up

--- config/config.js
+++ config/config.js
@@ -1,33 +1,20 @@
 
 var path = require('path'),
-    rootPath = path.normalize(__dirname + '/..'),
-    templatePath = path.normalize(__dirname + '/../app/mailer/templates'),
-    notifier = {
-      service: 'postmark',
-      APN: false,
-      email: false, // true
-      actions: ['comment'],
-      tplPath: templatePath,
-      key: 'POSTMARK_KEY',
-      parseAppId: 'PARSE_APP_ID',
-      parseApiKey: 'PARSE_MASTER_KEY'
-    };
+    rootPath = path.normalize(__dirname + '/..');
 
 module.exports = {
   development: {
     db: 'mongodb://localhost/mule_dev',
     root: rootPath,
-    notifier: notifier,
     app: {
-      name: 'Nodejs Express Mongoose Demo'
+      name: 'Mule Turnz Platform'
     }
   },
   test: {
     db: 'mongodb://localhost/mule_test',
     root: rootPath,
-    notifier: notifier,
     app: {
-      name: 'Nodejs Express Mongoose Demo'
+      name: 'Mule Turnz Platform'
     }
   },
   production: {}
