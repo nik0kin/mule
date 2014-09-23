7b4c0e1003212a5070019322575a9c0ddebcf0a6
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2014-09-22 20:21:05 -0500

    bg: oops forgot the scaling helper

---
+++ public/backgammon/modules/RenderHelper.js
@@ -0,0 +1,59 @@
+
+define(function () {
+  var that = {};
+
+  var scale, scaledViewPortSize
+
+  that.init = function (_scale, viewPortSize) {
+    scale = _scale;
+    scaledViewPortSize = {x: scale * viewPortSize.x, y: scale * viewPortSize.y};
+  };
+
+  that.createScaledBitmap = function (bitmapSrc, normalizedPosition) {
+    normalizedPosition = normalizedPosition || {x: 0, y: 0};
+
+    var newBitmap = new createjs.Bitmap(bitmapSrc);
+    newBitmap.x = normalizedPosition.x * scaledViewPortSize.x;
+    newBitmap.y = normalizedPosition.y * scaledViewPortSize.y;
+    newBitmap.scaleX = scale;
+    newBitmap.scaleY = scale;
+
+    return newBitmap;
+  };
+
+  that.createScaledBitmapAndAddChild = function (bitmapSrc, normalizedPosition, stage) {
+    var newBitmap = that.createScaledBitmap(bitmapSrc, normalizedPosition);
+    stage.addChild(newBitmap);
+
+    return newBitmap;
+  };
+
+  that.drawDebugRect = function (normalizedRect, stage) {
+    var shape = new createjs.Shape(),
+      x = normalizedRect.x * scaledViewPortSize.x,
+      y = normalizedRect.y * scaledViewPortSize.y,
+      width = normalizedRect.w * scaledViewPortSize.x,
+      height = normalizedRect.h * scaledViewPortSize.y;
+    shape.graphics.beginFill("#ff0000").drawRect(x, y, width, height);
+    stage.addChild(shape);
+  };
+
+  var isBetween = function (num, scaledMaxNum, low, high) {
+    var normalizedNum = num / scaledMaxNum;
+    return normalizedNum >= low && normalizedNum <= high;
+  };
+
+  that.isWithinScaledRect = function (clickPos, rect) {
+    return isBetween(clickPos.x, scaledViewPortSize.x, rect.x, rect.x + rect.w)
+        && isBetween(clickPos.y, scaledViewPortSize.y, rect.y, rect.y + rect.h);
+  };
+
+  that.getScaledPos = function (x, y) {
+    return {
+      x: x * scaledViewPortSize.x,
+      y: y * scaledViewPortSize.y
+    };
+  };
+
+  return that;
+});
